f59653c513614996d31b4cc8264023bdeff7e207
Philip Mirau <philip.mirau@gmail.com>
2021-12-30 16:32:03 +0100

    refactor: enhance css-variable usage

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import './styles/globals.css'
+
 export { default as Input } from './components/inputs/core/Input'
 export { default as InputSelector } from './components/inputs/InputSelector'
 export { default as useForm } from './hooks/useForm'
