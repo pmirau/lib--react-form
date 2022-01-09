e7c5c92526ff8e5744210c256e060e6826c5ddd9
Philip Mirau <philip.mirau@gmail.com>
2022-01-09 12:22:09 +0100

    fix: export correct input component

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import './styles/globals.css'
 
-export { default as Input } from './components/inputs/core/Input'
+export { default as Input } from './components/inputs/Input'
 export { default as InputSelector } from './components/inputs/InputSelector'
 export { default as useForm } from './hooks/useForm'
 
