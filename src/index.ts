81a8fc1812e5ecba2e9ddbef04c4395d8db921ed
Philip Mirau <philip.mirau@gmail.com>
2022-01-09 12:31:22 +0100

    fix: export previously created checkbox components

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import './styles/globals.css'
 
 export { default as Input } from './components/inputs/Input'
+export { default as Checkbox } from './components/inputs/Checkbox'
+export { default as CheckboxGroup } from './components/inputs/CheckboxGroup'
 export { default as InputSelector } from './components/inputs/InputSelector'
 export { default as useForm } from './hooks/useForm'
 
